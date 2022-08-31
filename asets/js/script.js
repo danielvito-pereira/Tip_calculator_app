3f11f54b950d39dc063da23e035bec5f92f1bdce
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2022-08-31 15:02:22 -0300

    teste

---
+++ asets/js/script.js
@@ -0,0 +1,21 @@
+var bill = document.getElementById('bill').value;
+var people = document.getElementById('people').value;
+var amount = document.getElementById('amount').innerHTML = amount;
+var total = document.getElementById("total").innerHTML = total;
+
+function calculate() {
+  var total = bill / percentagem;
+  return = total;
+}
+function mudarType() {
+ document.getElementById ("changed"). type = "number"; 
+}
+
+function resultado() {
+  if (resultado === 0) {
+    total.document
+  }
+}
+
+
+mudarType();
