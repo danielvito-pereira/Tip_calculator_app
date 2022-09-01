662e4d8415ac254e15ec6750bdb227c46c7fdb9b
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2022-09-01 11:37:34 -0300

    fim_do_estilo

--- asets/js/script.js
+++ asets/js/script.js
@@ -1,21 +1,38 @@
-var bill = document.getElementById('bill').value;
-var people = document.getElementById('people').value;
+
 var amount = document.getElementById('amount').innerHTML = amount;
 var total = document.getElementById("total").innerHTML = total;
 
-function calculate() {
-  var total = bill / percentagem;
-  return = total;
+
+function calcular() {
+  var bill = document.getElementById('bill').value;
+  var people = document.getElementById('people').value;
 }
+
+function teste() {
+
+  if (total === undefined) {
+    total=0;
+    document.getElementById("total").innerHTML = total;
+  } else if (amount === undefined) {
+    amount= 0;
+    document.getElementById('amount').innerHTML = amount;
+  }
+}
+
 function mudarType() {
  document.getElementById ("changed"). type = "number"; 
 }
 
 function resultado() {
-  if (resultado === 0) {
-    total.document
-  }
+  document.getElementById("total").innerHTML = total;
+  document.getElementById('amount').innerHTML = amount;
 }
 
+function reset() {
+  window.location.reload();
+}
 
-mudarType();
+teste();
+
+
+console.log(teste());
